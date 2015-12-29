24291056d5f41b36c64571cd79219bdb7b60678f
Steve <steve1.rm@gmail.com>
2015-12-29 23:07:50 +0700

    tvLevel.getParent().requestLayout()

--- app/src/main/java/androidbox/me/linkedin/MainFragment.java
+++ app/src/main/java/androidbox/me/linkedin/MainFragment.java
@@ -1,16 +1,11 @@
 package androidbox.me.linkedin;
 
-
+import android.app.Fragment;
 import android.os.Build;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import de.hdodenhof.circleimageview.CircleImageView;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,29 +22,13 @@ public class MainFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        TextView tvLevel = (TextView)view.findViewById(R.id.tvLevel);
-        CircleImageView circleImageView = (CircleImageView)view.findViewById(R.id.civProfilePic);
-
         View photoHeader = view.findViewById(R.id.photoHeader);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             photoHeader.setTranslationZ(6);
             photoHeader.invalidate();
-
-  //          circleImageView.setTranslationZ(4);
-    //        circleImageView.invalidate();
-
-   //         tvLevel.setTranslationZ(4);
-  //          tvLevel.bringToFront();
-    //        tvLevel.requestLayout();
-    //        tvLevel.invalidate();
         }
 
-/*
-        tvLevel.bringToFront();
-        tvLevel.requestLayout();
-        tvLevel.invalidate();
-*/
 
         return view;
     }
